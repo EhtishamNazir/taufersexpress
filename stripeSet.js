aff9d6ce93e229c9d83102e49e85996d2cad3283
EhtishamNazir <ehtishamnazir28@gmail.com>
2023-07-12 23:44:02 +0500

    Live API Added

--- stripeSet.js
+++ stripeSet.js
@@ -1,5 +1,5 @@
 import Stripe from 'stripe';
 
-const stripe = new Stripe('sk_test_51NKyRjIgKgg2nUGt8oS50WfH44M5FTlFIwv6WnBopArQ5JaaXrG9fPGXAPWLQK5UUhIcGj8vLJtwANlrkcL3lnRK00mGhikFzT');
+const stripe = new Stripe('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');
 
 export default stripe;
