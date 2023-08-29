58507db1e245ade7f04570b3f050d9afc11e31f5
EhtishamNazir <ehtishamnazir28@gmail.com>
2023-08-29 11:16:22 +0500

    Order Status Emptiness fixed

--- pages/api/updateOrder/[id].js
+++ pages/api/updateOrder/[id].js
@@ -15,16 +15,19 @@ export default async function handler(req, res) {
 
             try {
 
-                console.log(orderId);
-                console.log(newStatus);
-
-                if (!newStatus) {
-                    return res.status(422).json({ error: "Fill the status field" })
+                if (!newStatus || typeof (newStatus) === 'object') {
+                    return res.status(422).json({ error: "Fill the status field" });
                 }
 
-                const updatedOrder = await db.collection('Order').updateOne({ _id: new ObjectId(orderId) }, { $set: { orderStatus: newStatus } })
+                else if (newStatus === 1) {
+                    return res.status(422).json({ error: "Status is already 1 choose another value" });
+                }
 
-                res.status(201).json(updatedOrder)
+                else {
+                    const updatedOrder = await db.collection('Order').updateOne({ _id: new ObjectId(orderId) }, { $set: { orderStatus: newStatus } })
+
+                    res.status(201).json(updatedOrder)
+                }
             } catch (error) {
                 console.log(error);
                 res.status(500).json({ msg: "Error, check console." });
